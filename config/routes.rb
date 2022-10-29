205d67b6fd9f131fe4974b0dc51adabfd496fda3
Samer Sharba <samer.sharba@gmail.com>
2022-10-29 14:27:22 -0500

    create people index page json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'careers/index'
-  get 'addresses/index'
-  get 'phones/index'
-  get 'people/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	get '/people', :to => 'people#index'
 end
