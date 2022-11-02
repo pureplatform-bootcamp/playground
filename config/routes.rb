c155cc1cf1dfbb9606dc053d9ce4550cf9f87d87
beiancaa <beiancayousif89@gmail.com>
2022-11-02 15:45:59 +0300

    Adding book model and create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'addresses/index'
   get 'phones/index'
   get 'people/index'
+
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
