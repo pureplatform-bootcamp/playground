407f56a775c1cfb169874c8702754ac83c168364
Mustafa Amer <mamer.ma1234@gmail.com>
2022-11-03 12:46:25 +0300

    Update Action completed

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,5 @@
 class AddressesController < ApplicationController
+# Actions
   def create
     address = Address.new(address_params)
     unless address.save
@@ -7,7 +8,22 @@ class AddressesController < ApplicationController
     render json: {success: true, address: address}, status: :ok
   end
 
+  def update
+    address = Address.find(params[:id])
+    unless address == true
+      raise address.errors.full_messages
+    end
+      address.update(
+        person_id: params[:address][:person_id],
+        address1: params[:address][:address1],
+        city: params[:address][:city],
+        state: params[:address][:state],
+        zip_code: params[:address][:zip_code])
+        render json: {success: true, address: address}, status: :ok
+  end
 
+
+# Params
 private
   def address_params
     params.require(:address).permit(:person_id, :address1, :city, :state, :zip_code)
