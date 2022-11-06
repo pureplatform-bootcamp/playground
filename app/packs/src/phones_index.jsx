ed63d416b85d6d349bbdc05b594d22a84f08ddd0
Mustafa Amer <mamer.ma1234@gmail.com>
2022-11-07 00:04:25 +0300

    Show Phones in react page

---
+++ app/packs/src/phones_index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Table from "./shared/tables/material_table";
+
+export default class PhonesIndex extends React.Component {
+  render() {
+    const columns = [
+      { title: "Person ID", field: "person_id" },
+      { title: "Phone Number", field: "phone_number" },
+      { title: "Phone Type", field: "phone_type" },
+      { title: "Primary", field: "primary" },
+    ];
+
+    return (
+      <Table
+        title={"Phones"}
+        columns={columns}
+        options={{ filtering: false }}
+        data={(query) =>
+          new Promise((resolve, reject) => {
+            var filters;
+            if (query.filters.length > 0) {
+              filters = query.filters.map((col) => ({
+                field: col.column.field,
+                value: col.value,
+              }));
+            }
+            let url = `/phones.json?`;
+            url += "per_page=" + query.pageSize;
+            url += "&page=" + (query.page + 1);
+            if (query.filters.length > 0) {
+              url += "&filters=" + JSON.stringify(filters);
+            }
+            if (query.orderBy) {
+              url += "&orderBy=" + query.orderBy.field;
+              url += "&orderDirection=" + query.orderDirection;
+            }
+            if (query.search) {
+              url += "&search=" + query.search;
+            }
+            fetch(url)
+              .then((response) => response.json())
+              .then((result) => {
+                resolve({
+                  data: result.people,
+                  page: result.page - 1,
+                  totalCount: result.total,
+                });
+                window.breadcrumb.addBreadcrumb(result.breadcrumb);
+              });
+          })
+        }
+      />
+    );
+  }
+}
--- app/views/phones/_phone.json.jbuilder
+++ app/views/phones/_phone.json.jbuilder
@@ -1,4 +1,3 @@
-json.id p.id
 json.person_id p.person_id
 json.phone_number p.phone_number
 json.phone_type p.phone_type
