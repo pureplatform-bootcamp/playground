0b07b73111974d1806ef1d94ab71c505a00626c7
Mustafa Amer <mamer.ma1234@gmail.com>
2022-11-06 23:46:39 +0300

    Phones Json View

---
+++ app/views/phones/_phone.json.jbuilder
@@ -0,0 +1,5 @@
+json.id p.id
+json.person_id p.person_id
+json.phone_number p.phone_number
+json.phone_type p.phone_type
+json.primary p.primary
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :addresses, only: [:index, :create, :update, :destroy]
-  resources :phones, only: [:index, :create, :update]
+  resources :phones
   resources :people, only: [:index, :create] do
     collection do
       get :dead
