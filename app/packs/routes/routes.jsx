de1b3683e985205aa0ea41ad5680056803c7edfe
fryalasker <fryalw.asker00@gmail.com>
2022-11-07 19:21:31 +0300

    Careers Views

--- app/packs/routes/routes.jsx
+++ app/packs/routes/routes.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import PeopleIndex from '../src/people_index'
+import CareersIndex from '../src/career_index'
 
 export default (
   <Router>
     <div className='d-flex'>
       <Switch>
         <Route path='/people' exact component={PeopleIndex}/>
+        <Route path='/careers' exact component={CareersIndex}/>
       </Switch>
 
     </div>
   </Router>
 );
+
---
+++ app/packs/src/career_index.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Table from './shared/tables/material_table';
+
+export default class CareersIndex extends React.Component  {
+
+	render() {
+		const columns = [
+			{ title: 'Person id', field: 'person_id' },
+			{ title: 'Company name', field: 'company_name' },
+			{ title: 'Industry', field: 'industry' },
+			{ title: 'Title', field: 'title' },
+			{ title: 'Date started', field: 'date_started' },
+			{ title: 'Annual wage', field: 'annual_wage' },
+			{ title: 'Date ended', field: 'date_ended' },
+		]
+		return (
+			<Table
+				title={'Careers'}
+				columns={columns}
+				options={{filtering: false}}
+				data={query =>
+					new Promise((resolve, reject) => {
+						var filters
+						if (query.filters.length > 0) {
+							filters = query.filters.map((col) => ({field: col.column.field, value: col.value}))
+						}
+						let url = `/careers.json?`
+						url += 'per_page=' + query.pageSize
+						url += '&page=' + (query.page + 1)
+						if (query.filters.length > 0) {
+							url += '&filters=' + JSON.stringify(filters)
+						}
+						if (query.orderBy) {
+							url += '&orderBy=' + query.orderBy.field
+							url += '&orderDirection=' + (query.orderDirection)
+						}
+						if (query.search) {
+							url += '&search=' + (query.search)
+						}
+						fetch(url)
+							.then(response => response.json())
+							.then((result) => {
+								resolve({
+									data: result.people,
+									page: result.page - 1,
+									totalCount: result.total,
+								});
+								window.breadcrumb.addBreadcrumb(result.breadcrumb);
+							})
+					})
+				}
+			/>
+		)
+	}
+}
