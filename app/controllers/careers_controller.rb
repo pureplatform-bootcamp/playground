1b649b3c7c3ebc07c118958498efb28f776ddaa1
fryalasker <fryalw.asker00@gmail.com>
2022-11-07 19:05:53 +0300

    Career destroy action

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -10,9 +10,16 @@ class CareersController < ApplicationController
         end
         render json: {success: true, career: career}, status: :ok
       end
+
+      def destroy
+        @career = Career.find(params[:id])
+        @career.destroy
+        render json: {success: true}, status: :ok
+      end
+
 private
-def career_params
-    params.require(:career).permit(:person_id, :company_name, :industry, :title, :date_started)
-end
+      def career_params
+      params.require(:career).permit(:person_id, :company_name, :industry, :title, :date_started)
+      end
 end
 
