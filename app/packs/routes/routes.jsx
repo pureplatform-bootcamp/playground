3c188acc828e63fee3006adba78e45d9a088a336
Samer Sharba <samer.sharba@gmail.com>
2022-11-09 21:15:28 -0600

    fixing .src

--- app/packs/routes/routes.jsx
+++ app/packs/routes/routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PeopleIndex from "../src/people_index";
-import PhonesIndex from "../src/phones_index";
+import PeopleIndex from "src/people_index";
+import PhonesIndex from "src/phones_index";
 
 export default (
   <Router>
