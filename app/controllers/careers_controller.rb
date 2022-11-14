dd068532a579fc6562c351c9fb314860972d3894
fryalasker <fryalw.asker00@gmail.com>
2022-11-14 23:20:37 +0300

    Create, delete and update DONE

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,34 +1,34 @@
 class CareersController < ApplicationController
-    def index
-    @career = Career.all
+  def index
+  @career = Career.all
+  end
+
+  #create careers
+  def create
+      career = Career.new(career_params)
+      unless career.save
+        raise career.errors.full_messages
+      end
+      render json: {success: true, career: career}, status: :ok
     end
-    
-    #create careers
-    def create
-        @career = Career.new(career_params)
-        unless @career.save
-          raise @career.errors.full_messages
-        end
-        render json: {success: true, career: career}, status: :ok
-      end
 
-      #destroy careers
-      def destroy
-        @career = Career.find(params[:id])
-        @career.destroy
-        render json: {success: true}, status: :ok
-      end
+    #destroy careers
+    def destroy
+      career = Career.find(params[:id])
+      career.destroy
+      render json: {success: true}, status: :ok
+    end
 
-      #update careers
-      def update
-        career = Career.find(params[:id])
-        Career.update(career_params)
-        render json: { success: true }, status: :ok
-      end
+    #update careers
+    def update
+      career = Career.find(params[:id])
+      career.update(career_params)
+      render json: { success: true }, status: :ok
+    end
+
 
 private
 def career_params
-    params.require(:career).permit(:person_id, :company_name, :industry, :title, :date_started)
+  params.require(:career).permit(:person_id, :company_name, :industry, :title, :date_started)
 end
 end
-
--- app/javascript/components/careers_index.jsx
+++ app/javascript/components/careers_index.jsx
@@ -2,30 +2,71 @@ import React from 'react';
 import Table from './shared/tables/material_table';
 import $ from "jquery";
 
-export default class CareersIndex extends React.Component  {
-	handleCreate = (newData) => {
+export default class CareersIndex extends React.Component {
+	handleDelete = (oldData) => {
+		const data = oldData.id;
+		$.ajax({
+			url: "/careers/" + data,
+			type: "DELETE",
+			data: data,
+			dataType: "json",
+			success: function (response) {
+				console.log("success");
+			},
+			error: function (response) {
+				console.log(response.responseJSON.error);
+			},
+		});
+	};
+	handleUpdate = (newData, oldData) => {
+		let self = this;
+		const id = oldData.id;
 		const data = {
-		  careers: {
-			person_id: newData.person_id,
-			company_name: newData.company_name,
-			industry: newData.industry,
-			title: newData.title,
-			date_started: newData.date_started
-		  },
+			career: {
+				person_id: newData.person_id,
+				company_name: newData.company_name,
+				industry: newData.industry,
+				title: newData.title,
+				date_started: newData.date_started,
+			},
 		};
 		$.ajax({
-		  url: "/careers",
-		  type: "POST",
-		  data: data,
-		  dataType: "json",
-		  success: function (response) {
-			console.log("success");
-		  },
-		  error: function (response) {
-			console.log(response.responseJSON.error);
-		  },
+			url: "/careers/" + id,
+			type: "PATCH",
+			data: data,
+			dataType: "json",
+			success: function (response) {
+				console.log("success");
+			},
+			error: function (response) {
+				console.log(response.responseJSON.error);
+			},
 		});
-	  };
+	};
+	handleCreate = (newData) => {
+		let self = this;
+		const data = {
+			career: {
+				person_id: newData.person_id,
+				company_name: newData.company_name,
+				industry: newData.industry,
+				title: newData.title,
+				date_started: newData.date_started,
+			},
+		};
+		$.ajax({
+			url: "/careers",
+			type: "POST",
+			data: data,
+			dataType: "json",
+			success: function (response) {
+				console.log("success");
+			},
+			error: function (response) {
+				console.log(response.responseJSON.error);
+			},
+		});
+	};
 	render() {
 		const columns = [
 			{ title: 'Person id', field: 'person_id' },
@@ -34,52 +75,67 @@ export default class CareersIndex extends React.Component  {
 			{ title: 'Title', field: 'title' },
 			{ title: 'Date started', field: 'date_started' },
 		]
-		return (
-			<Table
-				title={'Careers'}
-				columns={columns}
-				options={{filtering: false}}
-				editable={{
-					onRowAdd: (newData) =>
-                    new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                    this.handleCreate(newData);
-                    resolve();
-                  }, 1000);
-                 }),
-				}}
-				data={query =>
+		return (<Table
+			title={"Careers"}
+			columns={columns}
+			options={{ filtering: true, addRowPosition: "first" }}
+			editable={{
+				onRowAdd: (newData) =>
 					new Promise((resolve, reject) => {
-						var filters
-						if (query.filters.length > 0) {
-							filters = query.filters.map((col) => ({field: col.column.field, value: col.value}))
-						}
-						let url = `/careers.json?`
-						url += 'per_page=' + query.pageSize
-						url += '&page=' + (query.page + 1)
-						if (query.filters.length > 0) {
-							url += '&filters=' + JSON.stringify(filters)
-						}
-						if (query.orderBy) {
-							url += '&orderBy=' + query.orderBy.field
-							url += '&orderDirection=' + (query.orderDirection)
-						}
-						if (query.search) {
-							url += '&search=' + (query.search)
-						}
-						fetch(url)
-							.then(response => response.json())
-							.then((result) => {
-								resolve({
-									data: result.careers,
-									page: result.page - 1,
-									totalCount: result.total,
-								});
-								window.breadcrumb.addBreadcrumb(result.breadcrumb);
-							})
-					})
-				}
-			/>
-		)
+						setTimeout(() => {
+							this.handleCreate(newData);
+							resolve();
+						}, 500);
+					}),
+				onRowDelete: (oldData) =>
+					new Promise((resolve, reject) => {
+						setTimeout(() => {
+							this.handleDelete(oldData);
+							resolve();
+						}, 500);
+					}),
+				onRowUpdate: (newData, oldData) =>
+					new Promise((resolve, reject) => {
+						setTimeout(() => {
+							this.handleUpdate(newData, oldData);
+							resolve();
+						}, 500);
+					}),
+			}}
+			data={(query) =>
+				new Promise((resolve, reject) => {
+					var filters;
+					if (query.filters.length > 0) {
+						filters = query.filters.map((col) => ({
+							field: col.column.field,
+							value: col.value,
+						}));
+					}
+					let url = `/careers.json?`;
+					url += "per_page=" + query.pageSize;
+					url += "&page=" + (query.page + 1);
+					if (query.filters.length > 0) {
+						url += "&filters=" + JSON.stringify(filters);
+					}
+					if (query.orderBy) {
+						url += "&orderBy=" + query.orderBy.field;
+						url += "&orderDirection=" + query.orderDirection;
+					}
+					if (query.search) {
+						url += "&search=" + query.search;
+					}
+					fetch(url)
+						.then((response) => response.json())
+						.then((result) => {
+							resolve({
+								data: result.careers,
+								page: result.page - 1,
+								totalCount: result.total,
+							});
+						});
+				})
+			}
+		/>
+		);
 	}
 }
